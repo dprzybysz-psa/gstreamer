aae6d75a9c044c2736c8601219e1eb483943e490
Dominik Przybysz <d.przybysz@prometheus-sa.com>
2023-07-08 23:20:00 +0200

    Introduce proCompile.bat as proCompileExample.bat

---
+++ pro/proCompileExample.bat
@@ -0,0 +1,31 @@
+set PRO_MESON_DIR=c:\Users\user\AppData\Roaming\Python\Python311\Scripts\
+set PRO_NINJA_DIR=c:\Users\user\AppData\Roaming\Python\Python311\Scripts\
+set PRO_MINGW_DIR=p:\Qt\Tools\mingw810_64\bin\
+set PRO_GIT_DIR=C:\Program Files\Git\cmd
+
+set PRO_GST_VERSION=1.22.4
+set PRO_GST_DESCRIPTION=Default release for qt5
+
+set PRO_INSTALL_PREFIX=%cd%\pro_install
+set PRO_LOG=%PRO_INSTALL_PREFIX%\pro_gstreamer.txt
+
+set PATH=%PRO_MESON_DIR%;%PRO_NINJA_DIR%;%PRO_MINGW_DIR%;%PRO_GIT_DIR%
+
+
+md %PRO_INSTALL_PREFIX%
+
+echo Prometheus gstreamer >> %PRO_LOG%
+echo GstVersion: %PRO_GST_VERSION% >> %PRO_LOG%
+echo ProDescription: %PRO_GST_DESCRIPTION% >> %PRO_LOG%
+echo CompilatonStart: %date% %time% >> %PRO_LOG%
+
+meson setup --buildtype=release --prefix=%PRO_INSTALL_PREFIX% builddir
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+meson compile -C builddir
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+meson install -C builddir
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo CompilatonEnd: %date% %time% >> %PRO_LOG%
